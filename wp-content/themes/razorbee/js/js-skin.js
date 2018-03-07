1d057bb9c0458632361157dde7279fa6789877fe
shahidsh7443 <shahidsh7443@gmail.com>
2018-03-07 21:50:22 +0530

    Updated

---
+++ wp-content/themes/razorbee/js/js-skin.js
@@ -0,0 +1,13 @@
+    (function($){
+    $(document).ready(function() {
+        
+          
+        $('.parallax').each(function(){
+           $(this).parallax("50%", 0.4);
+        });
+
+                
+        
+    });
+    })(jQuery);
+     /*2018-02-11 13:06*//* JS Generator Execution Time: 0.00010800361633301 seconds */
